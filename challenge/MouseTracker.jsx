af4b6499296f1a34ce3c370cc0e871e6da0c5918
Lucien Lin <luciensday@gmail.com>
2024-02-14 23:21:35 +0000

    complete mosemove

--- challenge/MouseTracker.jsx
+++ challenge/MouseTracker.jsx
@@ -1,5 +1,25 @@
-function MouseTracker(props){
+import { useState, useEffect } from "react"
 
+
+function MouseTracker(){
+    const [coord, setCoord] = useState([0, 0])
+
+    useEffect(()=> {
+        function update ({clientX, clientY}){
+            setCoord([clientX, clientY])
+        }
+        window.addEventListener("mousemove", update); 
+        return () => window.removeEventListener("mousemove", update); 
+        
+    },[])
+    
+   const [x, y] = coord 
+
+    return (
+    <output> 
+     {x}, {y} 
+    </output>
+    )
 
 }
 
